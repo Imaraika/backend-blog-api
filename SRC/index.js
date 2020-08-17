dadc2736df43fd03f8e88cc3600176dc87b64e5c
Ange <ange.ingabire07@gmail.com>
2020-08-17 07:58:35 +0200

    babelsjs for Ec6 has been added and all features needed

--- SRC/index.js
+++ SRC/index.js
@@ -1,4 +1,13 @@
-var name = function sayHello(n){
-console.log('HEllo' + name);
-}
-sayHello('ange');
+import express from 'express';
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Welcome Ange')
+});
+app.listen(9000,function(){
+    console.log('app is listening on port 9000');
+});
+// function sayHello(name){
+// console.log('HEllo ' + name);
+// }
+// sayHello('ange')
+//     // console.log(window)
