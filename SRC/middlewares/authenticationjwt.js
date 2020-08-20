d0aec1394bd918905d31ff0ec31a30a69f79fbe5
Ange <ange.ingabire07@gmail.com>
2020-08-20 08:23:55 +0200

    the user is able to access the data from the token successfully

--- SRC/middlewares/authenticationjwt.js
+++ SRC/middlewares/authenticationjwt.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const authorize = require('./autorization.middlewares');
 
 const app = express();
 
@@ -9,35 +10,27 @@ export const getToken = (req, res) =>{
         id: "1",
         name : "ANGE"
     }
-    const token = jwt.sign(user,"my super secret key",(err, token) => {
+    const token = jwt.sign(user,"my super secret key",{ expiresIn: '30s'},(err, token) => {
         res.send(token);
     });
    
 }
- export const verifyToken = (req,res) =>{
 
-    res.json({
-        message: 'post created!!'
-    });
-    function  verifyToken(req, res, next) {
-        const bearHeader = req.headers['authorization'];
-        if(typeof bearHeader != 'undefined') {
-        
-        }else{
-            //Forbidden
-        res.sendStatus(403);
-        }
-        
-        
-        }
-}
-//FORMAT of TOKEN
-
-//verify token 
-
-
-export const getAPI =(req, res) => {
+export const getAPI = (req, res) => {
     res.json({
         message: 'welcome to the API'
     });
 }
+
+export const verifyToken = (req, res, next)=>{
+    const bearerHeader = req.headers['authorization'];
+    // check if bearer is undentified
+    if(typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else{
+        res.sendStatus(403);
+    }
+};
--- SRC/routes/authRoutes.js
+++ SRC/routes/authRoutes.js
@@ -1,11 +1,26 @@
 import express, { Router } from 'express'; 
 import { getAPI, verifyToken, getToken} from '../middlewares/authenticationjwt';
 import jwt from 'jsonwebtoken'
+const authorize = require('../middlewares/autorization.middlewares');
+
 
 const router = express.Router();
-router.get('/api', getAPI );
+router.get('/api',getAPI );
 router.post('/login', getToken);
 
-router.post('/postToken', verifyToken);
+router.post('/postsToken',verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else{
+            res.json({
+                message: 'Post created .....',
+                authData
+            });
+        }
+    })
+
+});
+
 
 export default router;
