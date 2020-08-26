7fce550188e9b2b0ad58ad7d9d46b02045a7d87f
Ange <ange.ingabire07@gmail.com>
2020-08-26 22:25:08 +0200

    testing on post the blogposts

--- SRC/routesTesting/users.route.test.js
+++ SRC/routesTesting/users.route.test.js
@@ -1,6 +1,8 @@
 import getAllUsers from '../controllersTesting/users.controllers.test';
+import createUser from '../controllersTesting/users.controllers.test';
 import express from 'express';
 const router = express.Router();
 
 router.get('/users', getAllUsers );
+router.post('/users', createUser);
  export default router;
--- test/user.test.js
+++ test/user.test.js
@@ -3,9 +3,9 @@ import chaihttp from 'chai-http';
 import  app  from "../SRC/index";
 
 const { expect } = chai;
-
+ //when the user wants to view the users
 chai.use(chaihttp);
-describe('Posts',() =>{
+describe('Get All Users',() =>{
     describe('GET /users', () =>{
         it('should return an array of the all users', (done) =>{
             chai.request(app)
@@ -20,6 +20,27 @@ describe('Posts',() =>{
                 done();
 
             });
+        });
+    });
+});
+//Testing the save Post expecting status 200 of success
+describe('POST /users', () =>{
+    it('saves the users', (done) =>{
+        chai.request(app)
+        .post('/users')
+        .send({
+            emsil:"ange@gmail.com",
+            password:"678908",
+            role:"admin"
+        })
+        .end((err, res) =>{
+            if(err) done(err);
+        
+            expect(res).to.have.status(200);
+            expect(res).to.be.an('object');
+            expect(res.body.status).to.deep.equals('success');
+          
+            done();
         });
     });
 });
