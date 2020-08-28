626922c843b1e938ecf4cc7d0124b7db39753e41
Ange <ange.ingabire07@gmail.com>
2020-08-28 08:35:20 +0200

    the mongoose dependency have been added to connect to mongodb

--- SRC/controllers/postsContr.js
+++ SRC/controllers/postsContr.js
@@ -2,6 +2,9 @@
 import posts from '../models/postsCollection';
 import { v4 as uuidv4 } from 'uuid';
 
+const mongoose = require('mongoose');
+const Product = require('../models/postsCollection');
+
 export const getAllPosts = (req, res) => {
     res.send(posts);
 };
@@ -18,8 +21,22 @@ export const createPost = (req, res) => {
         comment:[],
         author: req.body.author
     };
+    const product = new Product({
+        // _id:new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    });
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+
     posts.push(post);
     res.send(post);
+
 };
 
 export const deletePost=(req,res)=>{
