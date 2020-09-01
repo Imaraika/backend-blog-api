256a9dee7d6e085b6e164a771e889eecc43506cb
Ange <ange.ingabire07@gmail.com>
2020-09-02 01:46:00 +0200

    changes

--- SRC/controllersMongoDB/blogPost.controller.js
+++ SRC/controllersMongoDB/blogPost.controller.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 const BlogPost = require('../models/blogpost.collection');
 
+const getAllPostinDB = (req, res, next) => {
+    BlogPost.find()
+  
+    .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs)
+    })
+    .catch(err => {
+        console.log(err);
+    res.status(500).json({
+        error: err
+    });
+});
+}
  const createBloginDB = (req, res, next) =>{
     const blogpost = new BlogPost({
         _id:new mongoose.Types.ObjectId(),
@@ -33,7 +47,34 @@ const BlogPost = require('../models/blogpost.collection');
     });
     });
 }
+const updatePostinDB = (req, res, next) => {
+    const id = req.params.id;
+    BlogPost.updateOne({ _id: id } , {$set: {title:req.body.newTitle, content: req.body.newContent, author: req.body.newAuthor}})
+    .exec().then( result => {
+        res.status(200).json(result);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+});
+}
+const deletePostindb = (req, res, next) => {
+    const id = req.params.id;
+    BlogPost.deleteOne({ _id: id }).exec().then( result => {
+        res.status(200).json(result);
+    }).catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
 module.exports = {
     createBloginDB,
     getSpecificPostinDB,
+    getAllPostinDB,
+    updatePostinDB,
+    deletePostindb
 };
